051b462afdf5e776c9f3cfcb7f64016854ef272d
Vaibhav Agrawal <agrvaibhav19@gmail.com>
2024-02-18 11:24:18 +0530

    corrected split_data.py, working

--- split_data.py
+++ split_data.py
@@ -15,15 +15,16 @@ os.mkdir(NEW_DIR)
 os.mkdir(osp.join(NEW_DIR, "train"))
 os.mkdir(osp.join(NEW_DIR, "val"))
 os.mkdir(osp.join(NEW_DIR, "test"))
-for class in tqdm(os.listdir(f"{ROOT_DIR}")):
+for classname in tqdm(os.listdir(f"{ROOT_DIR}")):
     for i in range(TRAIN):
-        shutil.copy(osp.join(ROOT_DIR, class, f"{i}.jpg"), osp.join(NEW_DIR, "train", f"{i}.jpg"))
-        shutil.copy(osp.join(ROOT_DIR, class, f"{i}_d.jpg"), osp.join(NEW_DIR, "train", f"{i}_d.jpg"))
+        shutil.copy(osp.join(ROOT_DIR, classname, f"{i}.jpg"), osp.join(NEW_DIR, "train", f"{i}.jpg"))
+        shutil.copy(osp.join(ROOT_DIR, classname, f"{i}_d.jpg"), osp.join(NEW_DIR, "train", f"{i}_d.jpg"))
         
     for i in range(VAL):
-        shutil.copy(osp.join(ROOT_DIR, class, f"{TRAIN + i}.jpg"), osp.join(NEW_DIR, "val", f"{i}.jpg"))
-        shutil.copy(osp.join(ROOT_DIR, class, f"{TRAIN + i}_d.jpg"), osp.join(NEW_DIR, "val", f"{i}_d.jpg"))
+        shutil.copy(osp.join(ROOT_DIR, classname, f"{TRAIN + i}.jpg"), osp.join(NEW_DIR, "val", f"{i}.jpg"))
+        shutil.copy(osp.join(ROOT_DIR, classname, f"{TRAIN + i}_d.jpg"), osp.join(NEW_DIR, "val", f"{i}_d.jpg"))
+
     for i in range(TEST):
-        shutil.copy(osp.join(ROOT_DIR, class, f"{TRAIN + VAL + i}.jpg"), osp.join(NEW_DIR, "test", f"{i}.jpg"))
-        shutil.copy(osp.join(ROOT_DIR, class, f"{TRAIN + VAL + i}_d.jpg"), osp.join(NEW_DIR, "test", f"{i}_d.jpg"))
+        shutil.copy(osp.join(ROOT_DIR, classname, f"{TRAIN + VAL + i}.jpg"), osp.join(NEW_DIR, "test", f"{i}.jpg"))
+        shutil.copy(osp.join(ROOT_DIR, classname, f"{TRAIN + VAL + i}_d.jpg"), osp.join(NEW_DIR, "test", f"{i}_d.jpg"))
 
